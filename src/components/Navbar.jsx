cd956f8aaf20e61859eeb8146a12076342ef8c3d
abhayabikramshahi <abhayabikramshahiofficial@gmail.com>
2025-09-12 19:12:59 +0545

    Changes in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+    <nav className='border border-[#1b1b1b] px-3 py-3'>
+        <div>
+            <div className="logo">
+                <h1 className='text-2xl font-bold tracking-tight text-green-500'>Nepal Coding House</h1>
+                <p className='mt-[4px]'>Learning Platform</p>
+            </div>
+        </div>
+    </nav>
+    </>
   )
 }
 
